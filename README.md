0b0064d9e27a1564c04783197a7ae56fa1bd67ca
Dhruv <91957341+Dhruv-099@users.noreply.github.com>
2024-10-05 22:05:58 +0530

    Basic steps

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# DR_Glucoma_AMD
+# DR Glucoma AMD
+ This is the capstone project based on the Eye disease detection of 3 diseases being Diabetic Retinopathy, Glucoma, AMD.
+ ###  Techstack used
+ 
  
---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import joblib
+from PIL import Image
+model = joblib.load('model.pkl')
+st.title("ML Model Dashboard")
+uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image.', use_column_width=True)
+    # Call your prediction function here
