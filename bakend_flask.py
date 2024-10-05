cbe22aec8e58000dc7b1dcc4ca0bae7c479b4936
Dhruv <91957341+Dhruv-099@users.noreply.github.com>
2024-10-05 22:32:02 +0530

    Updated basics

--- bakend_flask.py
+++ bakend_flask.py
@@ -1,19 +1,25 @@
 from flask import Flask, request
+'''
 import boto3
-
-app = Flask(__name__)
-
-@app.route('/upload', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
-        return "No file part"
-    
+        return "No file part"    
     file = request.files['file']
     if file.filename == '':
         return "No selected file"
-    
     # Upload to AWS S3
     s3 = boto3.client('s3')
     s3.upload_fileobj(file, 'your-bucket-name', file.filename)
     
     return "File uploaded successfully"
+'''
+
+app = Flask(__name__)
+@app.route("/")
+
+def re():
+    return render_template('home.html')
+
+if __name__=='main':
+    app.run(host='0.0.0.0', debug = True)
+
