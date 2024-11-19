8ca2875447d40867e97d3f45d53b1ed580fb203e
Dhruv <91957341+Dhruv-099@users.noreply.github.com>
2024-11-19 19:23:50 +0530

    Final change

--- bakend_flask.py
+++ bakend_flask.py
@@ -3,12 +3,13 @@ from website import create_app
 from website.views import views  
 
 app = create_app()
+app.config['DEBUG'] = False
 
 if 'views' not in app.blueprints:  
     app.register_blueprint(views, url_prefix="/")
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run()
 #if __name__ == '__main__':
 #    app.run(host='0.0.0.0', port=8080)
